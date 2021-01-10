c0b3c365e0a06af49a32d6f5e21d74d860c0ebf4
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-01-09 19:08:34 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hidaner Ferrer on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
